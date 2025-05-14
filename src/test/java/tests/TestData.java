25a345d05273423670052fa777b208db86af655b
Oleg <namozov@dev.vtb.ru>
2025-05-14 21:56:43 +0300

    optimized tests

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,11 +1,8 @@
 package tests;
 
 public class TestData {
-    public static String userName = "OLEG",
-            password = "Oleg!234";
-
     public static final String USERNAME = "OLEG";
     public static final String PASSWORD = "Oleg!234";
-    public static final String BOOK_ISBN = "9781449365035";
+    public static final String BOOK_ISBN = "9781593277574";
 
 }
