a1f5d75988ab6432c7ae71028283e906a1ad2409
Oleg <namozov@dev.vtb.ru>
2025-05-16 18:49:49 +0300

    added getProperty for TestData

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,8 +1,7 @@
 package tests;
 
 public class TestData {
-    public static final String USERNAME = "OLEG";
-    public static final String PASSWORD = "Oleg!234";
-    public static final String BOOK_ISBN = "9781593277574";
-
+    public static final String USERNAME = System.getProperty("USERNAME");
+    public static final String PASSWORD = System.getProperty("PASSWORD");
+    public static final String BOOK_ISBN = System.getProperty("BOOK_ISBN");
 }
